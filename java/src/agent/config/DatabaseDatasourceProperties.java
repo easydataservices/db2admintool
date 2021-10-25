30e4ebfed285f24027075cc4d97ca3cf9f1657f5
Jeremy Rickard <30393514+jrickard27@users.noreply.github.com>
2021-10-25 22:09:13 +1300

    Add files via upload

---
+++ java/src/agent/config/DatabaseDatasourceProperties.java
@@ -0,0 +1,16 @@
+package com.easydataservices.db2admintool.agent.config;
+
+import com.easydataservices.db2admintool.agent.config.DatasourceProperties;
+
+//------------------------------------------------------------------------------
+// File:         DatabaseDatasourceProperties.java
+// Licence:      Apache License 2.0
+// Description:  
+/**
+ * Interface for (source) database datasource properties.
+ * @author jeremy.rickard@easydataservices.com
+ * @version 2021.10.23
+ */ 
+//------------------------------------------------------------------------------
+public interface DatabaseDatasourceProperties extends DatasourceProperties {
+}
