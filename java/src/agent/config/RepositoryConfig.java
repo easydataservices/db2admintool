378410d340324e6f3d12c9fa8858847a8241af71
Jeremy Rickard <30393514+jrickard27@users.noreply.github.com>
2021-10-25 22:08:56 +1300

    Update RepositoryConfig.java

--- java/src/agent/config/RepositoryConfig.java
+++ java/src/agent/config/RepositoryConfig.java
@@ -1,6 +1,5 @@
 package com.easydataservices.db2admintool.agent.config;
 
-import java.sql.SQLException;
 import javax.sql.DataSource;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,29 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import com.easydataservices.db2admintool.agent.config.RepositoryDatasourceProperties;
 
 //------------------------------------------------------------------------------
 // File:         RepositoryConfig.java
-// Licence:      GNU General Public License v3.0
+// Licence:      Apache License 2.0
 // Description:  
 /**
  * Configuration of repository datasource and JDBC template.
  * @author jeremy.rickard@easydataservices.com
- * @version 2021.10.17
+ * @version 2021.10.23
  */ 
 //------------------------------------------------------------------------------
 @Configuration
-@PropertySource("file:repository.properties")
 public class RepositoryConfig {
   private static final String className = RepositoryConfig.class.getName();
   private static final Logger logger = Logger.getLogger(className);
 
   @Autowired
-  private Environment env;
+  private RepositoryDatasourceProperties properties;
 
   /**
    * Configure repository data source.
@@ -41,16 +38,16 @@ public class RepositoryConfig {
     final DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
     logger.entering(this.className, method);
-    dataSource.setDriverClassName(env.getProperty("repoDriverClassName"));
-    dataSource.setUrl(env.getProperty("repoConnectionUrl"));
-    if (env.getProperty("repoUserName") != null) {
-      dataSource.setUsername(env.getProperty("repoUserName"));
-      dataSource.setPassword(env.getProperty("repoPassword"));
+    dataSource.setDriverClassName(properties.getDriverClassName());
+    dataSource.setUrl(properties.getConnectionUrl());
+    if (properties.getUserName() != null) {
+      dataSource.setUsername(properties.getUserName());
+      dataSource.setPassword(properties.getPassword());
     }
-    if (env.getProperty("repoSchema") != null) {
-      dataSource.setSchema(env.getProperty("repoSchema"));
+    if (properties.getSchemaName() != null) {
+      dataSource.setSchema(properties.getSchemaName());
     }
-    logger.exiting(this.className, method);
+    logger.exiting(this.className, method, dataSource);
     return dataSource;
   }
 
@@ -60,9 +57,11 @@ public class RepositoryConfig {
   @Bean("repoJdbcTemplate")
   public JdbcTemplate getJdbcTemplate(@Qualifier("repoDataSource") DataSource dataSource) {
     final String method = "getJdbcTemplate";
+    JdbcTemplate jdbcTemplate;
 
-    logger.entering(this.className, method);
-    logger.exiting(this.className, method);
-    return new JdbcTemplate(dataSource);
+    logger.entering(this.className, method, new Object[] {dataSource});
+    jdbcTemplate = new JdbcTemplate(dataSource);
+    logger.exiting(this.className, method, jdbcTemplate);
+    return jdbcTemplate;
   }
 }
