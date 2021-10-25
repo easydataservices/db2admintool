f34ef7f1691c416c53e5ed204819eeda47ae9bb8
Jeremy Rickard <30393514+jrickard27@users.noreply.github.com>
2021-10-25 21:57:43 +1300

    Create module_transform.sql

---
+++ database/schemata/ctl_db2luw/modules/module_transform.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------------------------------------------------------
+-- Script:      module_transform.sql
+-- Licence:     GNU General Public License v3.0
+-- Author:      jeremy.rickard@easydataservices.com
+-- Version:     2021.10.12
+--
+-- Description: Module CTL_DB2LUW.TRANSFORM (transform Db2 LUW snapshots into repository)
+--------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE MODULE ctl_db2luw.transform;
+
+ALTER MODULE ctl_db2luw.transform
+PUBLISH PROCEDURE transform_sndbcf();
